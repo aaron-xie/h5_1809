8ff228dbaaa56c2661fb6e03c04e71dcfff65769
aaron <jeroo@qq.com>
2019-01-09 16:07:30 +0800

    React组件

---
+++ React/app/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Card from './components/Card';
+
+class App extends React.Component{
+    render(){
+        return (
+            <div>
+                App
+                <Card/>    
+            </div>
+        )
+    }
+}
+
+export default App;
